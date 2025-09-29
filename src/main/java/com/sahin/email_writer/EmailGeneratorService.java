d1b8c0211f3e6ff139736d2ef454efab764dc02d
sahin Islam <sahinislam878@gmail.com>
2025-09-30 01:38:03 +0530

    Completed buildPrompt Logic

--- src/main/java/com/sahin/email_writer/EmailGeneratorService.java
+++ src/main/java/com/sahin/email_writer/EmailGeneratorService.java
@@ -6,8 +6,20 @@ import org.springframework.stereotype.Service;
 public class EmailGeneratorService {
     public String generateEmailReply(EmailRequest emailRequest) {
         //Build Prompt
+        String prompt = buildPrompt(emailRequest);
         //Prepare RAW JSON Body
         //Send Request
         //Extract Response
+    }
+
+    private String buildPrompt(EmailRequest emailRequest) {
+        StringBuilder prompt = new StringBuilder();
+        prompt.append("Generate a Professional email reply for the following email : ");
+        if (emailRequest.getTone() != null && !(emailRequest.getTone().isEmpty())){
+            prompt.append("use a ").append(emailRequest.getTone()).append(" tone");
+        }
+        prompt.append("Original Email : \n").append(emailRequest.getEmailContent());
+
+        return prompt.toString();
     }
 }
